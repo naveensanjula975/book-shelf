0e088f532dd6fb80fd8847a4dbe0b8a020468e76
naveen sanjula <82176749+naveensanjula975@users.noreply.github.com>
2025-11-14 21:05:42 +0530

    Add REST framework and JWT

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('books.urls')),
+urlpatterns += [
+    path("api/auth/token/", TokenObtainPairView.as_view()),
+    path("api/auth/token/refresh/", TokenRefreshView.as_view()),
 ]
--- backend/books/admin.py
+++ backend/books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id","title","author","year","isbn","created_at")
+    search_fields = ("title","author","isbn")
+    list_filter = ("year",)
